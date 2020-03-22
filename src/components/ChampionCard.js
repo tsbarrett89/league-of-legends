419c6828d3a68958b23c0631e6989c18426c242c
tsbarrett89 <tsbarrett89@gmail.com>
2020-03-22 11:56:20 -0600

    added img in ChampionCard

--- src/components/ChampionCard.js
+++ src/components/ChampionCard.js
@@ -5,6 +5,7 @@ const ChampionCard = props => {
     return (
         <div>
             <h3>{props.champion.name}</h3>
+            <img src={props.champion.image.sprite} />
         </div>
     )
 }
